bb2d82a1f6523d51acbee9737facd1f9c99e467a
lew1scho1 <lewis.choi.ssc@gmail.com>
2021-03-31 17:03:17 -0500

    Initial commit

---
+++ bubble/2_background.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()  # 초기화 (반드시 필요)
+
+#화면 크기 설정
+screen_width = 480  #가로 크기
+screen_height = 640  #세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#화면 타이틀 설정
+pygame.display.set_caption("Nado Game")  #게임 이름
+
+# 배경 이미지 불러오기
+background = pygame.image.load("C:\\Users\\lenovo\\PycharmProjects\\untitled1\\bubble\\background.png")
+
+# 이벤트 루프
+running = True #게임이 진행중인가?
+while running:
+    for event in pygame.event.get(): # 어떤 이벤트가 발생했는가?
+        if event.type == pygame.QUIT: # x버튼을 눌러서 껐을 때
+            running = False #창이 닫히는 이벤트가 발생
+
+    screen.blit(background, (0, 0)) # 배경 그리기
+
+    pygame.display.update()  #게임 화면 다시 그리고. 반드시 계속 다시 호출 되어야 함.
+
+# pygame 종료
+pygame.quit()
+
+
+
